41b954556309c51fff541b183248ff001c4fd302
Piotr Barański <piotreeeczek@gmail.com>
2019-08-27 11:07:09 +0200

    module exports update

--- test.js
+++ test.js
@@ -3,7 +3,9 @@ const chance = require('chance').Chance();
 let userid = chance.bb_pin().substr(0,7)
 let token = chance.apple_token()
 
-module.exports=userid
-module.exports=token
+module.exports={
+    userid,
+    token
+}
 
-//console.log(userid,token)
+//console.log(module.exports)
