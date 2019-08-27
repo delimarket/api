6f92bc736aa1a6c3d19a4849b9ce123a119caba5
Piotr Barański <piotreeeczek@gmail.com>
2019-08-27 11:57:12 +0200

    tidy code & login

--- src/hash.js
+++ src/hash.js
@@ -4,6 +4,14 @@ const hash = (password) => {
     return sha('sha256').update(password).digest('hex')
 }
 
-//console.log(hash('admin'))
+const check_hash = (plain,hashed) => {
+    if(hash(plain)==hashed) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(check_hash("password","5e884s898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8"))
 
 module.exports=hash;
