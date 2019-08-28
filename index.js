8d95df12a392945b47763b8d4a9c82f7135f06fc
AdamSiekierski <adam.siekiera@outlook.com>
2019-08-28 14:48:56 +0200

    Fixes

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const session = require('express-session')
 const FileStore = require('session-file-store')(session)
-const chance = require('chance').Chance()
+const { check, validationResult } = require('express-validator')
+const validator = require('validator')
 
+const chance = require('chance').Chance()
 const sha = require('sha.js')
 const models = require('./models')
 const valid = require("./validators/form")
@@ -15,7 +17,7 @@ const port = process.env.PORT || 8080
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cookieParser())
-app.use(session({ secret: `no way you will guess it`, resave: false, store: new FileStore({ secret: `no way you will guess it`, logFn: args => { return args } }), saveUninitialized: true }))
+app.use(session({ secret: `no way you will guess it`, resave: true, store: new FileStore({ secret: `no way you will guess it`, logFn: args => { return args } }), saveUninitialized: true }))
 
 const router = express.Router();
 
@@ -25,88 +27,54 @@ router.get('/', (req, res) => {
   })
 })
 
-router.post('/login', async (req, res) => {
+router.post('/login', [check('phone').isMobilePhone(), check('password').isLength({ min: 6, max: 30 })], async (req, res) => {
   if (!req.session.user) {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() })
+    }
+
     let phone=`+48${req.body.phone}`
 
     let user = await models.User.where('phone', phone).fetch()
 
-    if (sha('sha256').update(req.body.password).digest('hex') === user.password) {
+    if (sha('sha256').update(req.body.password).digest('hex') === user.attributes.password) {
       req.session.user = user
-      res.redirect(`/userPage?loginRedirect=true`)
+      return res.redirect(`/userPage`)
     } else {
-      res.status(401).json({ message: 'invalid credits' })
+      return res.status(401).json({ message: 'invalid credits' })
     }
   } else {
-    res.redirect('/userPage')
+    return res.redirect('/userPage')
   }
 })
 
-router.post('/register', async (req,res) => {
-  let dane = ['phone','name','password']
-
-  const makemsg = valid.make
-
-  for (let d of dane) {
-    if(!req.body[d] || req.body[d]=="") {
-      res.status(401).json(makemsg(`Invalid ${d}`))
-      return
+router.post('/register', [
+  check('phone').isMobilePhone().custom((phone) => {
+    return models.User.where('phone', `+48${phone}`).fetchAll().then((users) => {
+      if (users.length !== 0) {
+        return Promise.reject(new Error('Phone already in use'))
+      }
+    })
+  }),
+  check('name').custom((name) => {
+      const fullName = name.split(" ");
+      if (validator.isAlpha(fullName[0]) && validator.isLength(fullName[0],{min:3,max:30}) && fullName[1]) {
+        return true
+      } else {
+        throw new Error('Invalid name or surname')
+      }
+  }),
+  check('password').isLength({ min: 6, max: 30 }),
+  check('is_deliver').isBoolean()
+], async (req,res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() })
     }
-  }
 
-  if(req.body['is_deliver']!=0 && req.body['is_deliver']!=1) {
-    res.status(401).json(makemsg('is deliver or not?'))
-    return
-  }
+    let password = sha('sha256').update(req.body.password).digest('hex')
 
-  //valid phone
-  if (!valid.v_phone(req.body.phone)) {
-    res.json(makemsg('invalid phone number'))
-    return
-  }
-
-  let phone = '+48'+req.body.phone;
-
-  //name
-  let name=valid.v_name(req.body['name'])
-
-  if(name.statut==true) {
-    name=name.word
-  } else {
-    res.json(makemsg('wrong name'))
-    return
-  }
-
-  //check & hash passwd
-  if (req.body.password.length<6) {
-    res.json(makemsg('too short password'))
-    return
-  }
-
-  let password = sha('sha256').update(req.body.password).digest('hex')
-
-  //generate id & token
-  let userid,usertoken,rows,phone_access
-
-  try {
-    do {
-      userid=chance.bb_pin()
-      rows = await models.User.where('id', userid).count()
-    } while(rows!=0)
-
-    do {
-      usertoken=chance.apple_token()
-      rows = await models.User.where('token', usertoken).count()
-    } while(rows!=0)
-
-    //check phone nr
-    phone_access=await models.User.where('phone',phone).count()
-
-  } catch (error) {
-    res.json(makemsg("dupa"))
-  }
-
-  if(phone_access==0) {
     let newuser = new models.User({
       'id':userid,
       'phone':phone,
@@ -118,15 +86,12 @@ router.post('/register', async (req,res) => {
     })
 
     newuser.save().then(() => {
-      res.status(200).json(makemsg('user ('+userid+') added', false))
+      res.redirect('/userpage')
     }).catch((error) => {
       res.json(makemsg(error))
     })
-  } else {
-    res.json(makemsg('your phone number is already registered'))
   }
-
-})
+)
 
 const authenticateUser = (req, res, next) => {
   if (req.session.user) {
@@ -139,7 +104,7 @@ const authenticateUser = (req, res, next) => {
 
 router.get('/userPage', authenticateUser, (req, res) => {
   res.json({
-    message: req.query.loginRedirect === "true" ? 'Successfully logged in': 'Welcome to user page',
+    message: 'Welcome to user page',
     user: req.session.user
   })
 })
