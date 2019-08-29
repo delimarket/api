d53a57f3deffb3c0be4c9b7c1fcc8efb6efdc317
AdamSiekierski <adam.siekiera@outlook.com>
2019-08-29 09:01:32 +0200

    Code formatters

--- router.js
+++ router.js
@@ -1,6 +1,6 @@
-const router = require('express').Router()
+const express = require('express')
+const router = express.Router()
 const { check, validationResult } = require('express-validator')
-const validator = require('validator')
 
 const sha = require('sha.js')
 const models = require('./models')
@@ -16,70 +16,90 @@ router.get('/', (req, res) => {
   })
 })
 
-router.post('/login', [check('phone').isMobilePhone(), check('password').isLength({ min: 6, max: 30 })], async (req, res) => {
-  if (!req.session.user) {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() })
-    }
+router.post(
+  '/login',
+  [
+    check('phone').isMobilePhone(),
+    check('password').isLength({ min: 6, max: 30 })
+  ],
+  async (req, res) => {
+    if (!req.session.user) {
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() })
+      }
 
-    let phone=`+48${req.body.phone}`
+      const phone = `+48${req.body.phone}`
+      const user = await models.User.where('phone', phone).fetch()
 
-    let user = await models.User.where('phone', phone).fetch()
-
-    if (sha('sha256').update(req.body.password).digest('hex') === user.attributes.password) {
-      req.session.user = user
-      return res.redirect(`/userPage`)
+      if (
+        sha('sha256')
+          .update(req.body.password)
+          .digest('hex') === user.attributes.password
+      ) {
+        req.session.user = user
+        return res.redirect(`/userPage`)
+      } else {
+        return res.status(401).json({ message: 'invalid credits' })
+      }
     } else {
-      return res.status(401).json({ message: 'invalid credits' })
+      return res.redirect('/userPage')
     }
-  } else {
-    return res.redirect('/userPage')
   }
-})
+)
 
-router.post('/register', [
-  check('phone').isMobilePhone().custom(isPhoneUsed),
-  check('name').custom(isFullName),
-  check('password').isLength({ min: 6, max: 30 }),
-  check('is_deliver').isBoolean()
-  ], async (req,res) => {
+router.post(
+  '/register',
+  [
+    check('phone')
+      .isMobilePhone()
+      .custom(isPhoneUsed),
+    check('name').custom(isFullName),
+    check('password').isLength({ min: 6, max: 30 }),
+    check('is_deliver').isBoolean()
+  ],
+  async (req, res) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() })
     }
 
-    let password = sha('sha256').update(req.body.password).digest('hex')
+    const password = sha('sha256')
+      .update(req.body.password)
+      .digest('hex')
     const userToken = userTokenGenerator()
 
     let userId, rows
     do {
       userId = chance.bb_pin()
-      rows = await models.User.where('id',userId).count()
+      rows = await models.User.where('id', userId).count()
     } while (rows > 0)
 
-    let newuser = new models.User({
-      'id': userId,
-      'phone': `+48${req.body.phone}`,
-      'name': req.body.name,
-      'password': password,
-      'is_deliver': parseInt(req.body.is_deliver),
-      'active': 1,
-      'token': userToken['hashed']
+    const newuser = new models.User({
+      id: userId,
+      phone: `+48${req.body.phone}`,
+      name: req.body.name,
+      password: password,
+      is_deliver: parseInt(req.body.is_deliver),
+      active: 1,
+      token: userToken.hashed
     })
 
-    newuser.save(null, {method: 'insert'}).then(() => {
-      return res.status(200).json({
-        'message': 'User created successfully',
-        'data': {
-          'id':userId,
-          'phone': `+48${req.body.phone}`,
-          'name': req.body.name
-        }
+    newuser
+      .save(null, { method: 'insert' })
+      .then(() => {
+        return res.status(200).json({
+          message: 'User created successfully',
+          data: {
+            id: userId,
+            phone: `+48${req.body.phone}`,
+            name: req.body.name
+          }
+        })
       })
-    }).catch((err) => {
-      console.log(err)
-    })
+      .catch(err => {
+        console.log(err)
+      })
   }
 )
 
