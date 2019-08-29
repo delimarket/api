c1bd244ff1a482349f221eb8c42c1f2ff24c0647
AdamSiekierski <adam.siekiera@outlook.com>
2019-08-29 09:01:32 +0200

    Code formatters

--- index.js
+++ index.js
@@ -12,9 +12,21 @@ const port = process.env.PORT || 8080
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cookieParser())
-app.use(session({ secret: `no way you will guess it`, resave: true, store: new FileStore({ secret: `no way you will guess it`, logFn: args => { return args } }), saveUninitialized: true }))
+app.use(
+  session({
+    secret: 'no way you will guess it',
+    resave: true,
+    store: new FileStore({
+      secret: 'no way you will guess it',
+      logFn: args => {
+        return args
+      }
+    }),
+    saveUninitialized: true
+  })
+)
 
 app.use('/', router)
 
 app.listen(port)
-console.log(`Listening on port: ${port}`);
+console.log(`Listening on port: ${port}`)
